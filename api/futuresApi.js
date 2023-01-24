a939aa1ed72ae200f77c99e6054b8c5cb2e5ecac
Vladimir <>
2023-01-24 22:59:57 +0300

    debug

--- api/futuresApi.js
+++ api/futuresApi.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
 async function getPairIndex(symbol) {
-  return await axios.get('https://fapi.binance.com/fapi/v1/premiumIndex', {
+  try {
+    console.log('1')
+  const res = await axios.get('https://fapi.binance.com/fapi/v1/premiumIndex', {
     params: {
       symbol: symbol
     }
   })
+  console.log('2', res)
+  return res
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 
