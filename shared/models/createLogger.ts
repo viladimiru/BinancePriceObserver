b2198c43f55929652e4bae33c2b9a0e64b10a108
Vladimir <bobahdro@gmail.com>
2023-11-19 23:28:37 +0300

    set max size for logs

--- shared/models/createLogger.ts
+++ shared/models/createLogger.ts
@@ -2,14 +2,20 @@ import winston from 'winston';
 
 const { timestamp, json, combine } = winston.format;
 
+const maxSize = 1024 * 200;
+
 export function createLogger(serviceName: string, path: string): winston.Logger {
 	return winston.createLogger({
 		level: 'info',
 		format: combine(timestamp(), json()),
 		defaultMeta: { service: serviceName },
 		transports: [
-			new winston.transports.File({ filename: path.concat('/error.log'), level: 'error' }),
-			new winston.transports.File({ filename: path.concat('/combined.log') }),
+			new winston.transports.File({
+				filename: path.concat('/error.log'),
+				level: 'error',
+				maxsize: maxSize,
+			}),
+			new winston.transports.File({ filename: path.concat('/combined.log'), maxsize: maxSize }),
 		],
 	});
 }
